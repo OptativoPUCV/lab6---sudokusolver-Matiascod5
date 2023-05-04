ec175d0a10e83eac902390ecac710b7c000220eb
Replit user <>
2023-05-04 23:37:14 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -107,7 +107,6 @@ List* get_adj_nodes(Node* n){
           if ( is_valid( b)){
             pushBack( list, b);
           }
-        
         }
         return list;
       }
@@ -117,7 +116,15 @@ List* get_adj_nodes(Node* n){
 }
 
 int is_final(Node* n){
-    return 0;
+
+  for ( int i = 0 ; i < 9 ; i++){
+    for ( int j = 0 ; j < 9 ; j++){
+      if ( n->sudo[i][j] == 0){
+        return = 0;
+      }
+    }
+  }
+  return 0;
 }
 
 Node* DFS(Node* initial, int* cont){
