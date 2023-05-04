1880aeb870153534a1676431d056d1a025f6269c
Replit user <>
2023-05-04 03:58:16 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -53,7 +53,8 @@ List* get_adj_nodes(Node* n){
   List* list=createList();
   int j;
   int h;
-  for( int i = 0; i < 9 ; i++){
+  int i
+  for( i = 0; i < 9 ; i++){
     Node* b = copy( n);
     j = 2 + i;
     h = i + 1;
